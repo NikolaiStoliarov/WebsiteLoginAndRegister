6a4679e19825e9c73f99ecce6494e7fa670cb6c1
Nikolai Stoliarov <nikolaistoliarovmail@gmail.com>
2023-07-22 00:11:59 +0300

    add (animation color link) and error (textbox)

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ registerLink.addEventListener('click', ()=> {wrapper.classList.add('active');});
 loginLink.addEventListener('click', ()=> {wrapper.classList.remove('active');});
 btnPopup.addEventListener('click', ()=> {wrapper.classList.add('active-popup');});
 iconClose.addEventListener('click', ()=> {wrapper.classList.remove('active-popup');});
+
+document.getElementById('submitBtn').addEventListener('click', function(event) {
+  var checkbox = document.getElementById('myCheckboxIAgree');
+  var errorMessage = document.getElementById('error-message');
+  
+  if (!checkbox.checked) {
+    event.preventDefault(); 
+    errorMessage.style.display = 'block'; 
+  } else {
+    errorMessage.style.display = 'none';
+  }
+});
+
+window.onload = function() { // Hide the error message when the page loads
+  document.getElementById('error-message').style.display = 'none';
+};
